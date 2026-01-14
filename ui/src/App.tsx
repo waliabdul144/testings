a5cc84108940e2b4cb2f0d7d741a486aa9b3fb4c
awali <awali@i2cinc.com>
2026-01-14 13:02:23 +0500

    github pages test

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -28,6 +28,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+
+      <p className="read-the-docs">
+        Now Testing Git Lab
+      </p>
     </>
   )
 }
