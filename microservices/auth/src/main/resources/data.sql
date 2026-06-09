e6dcaf493ed5919e338005b799a8704344205027
awali <awali@i2cinc.com>
2026-06-09 11:19:42 +0500

    all now

---
+++ microservices/auth/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (userid, name, email, password, address, phone, type)
+VALUES
+    ('admin01', 'Admin User', 'admin@test.com', 'admin123', 'Karachi', '03001234567', 'ADMIN'),
+    ('user01', 'Normal User', 'user@test.com', 'user123', 'Lahore', '03111234567', 'USER');
